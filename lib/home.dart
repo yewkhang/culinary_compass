4f78f766c9914699831e93a55e4ccb3547ad07c0
yewkhang <yewkhang45@outlook.com>
2024-05-20 14:42:28 +0800

    changed class name to HomePage

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
