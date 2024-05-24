86143581a67ed6eb0ebe8e2b6ad12e52574a0c68
yewkhang <yewkhang45@outlook.com>
2024-05-25 00:00:33 +0800

    updated page from stateless to stateful

--- lib/logging_page.dart
+++ lib/logging_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -6,55 +5,67 @@ import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io'; // for reading and writing files
 
-class LoggingPage extends StatelessWidget {
+class LoggingPage extends StatefulWidget {
   const LoggingPage({super.key});
 
+  @override
+  State<LoggingPage> createState() => _LoggingPageState();
+}
+
+class _LoggingPageState extends State<LoggingPage> {
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(ImageController());
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(children: [
-              // spot for image to show
-              Obx(() => controller.selectedImagePath.value == ''
-                  ? Container(width: 450, height: 400, color: Colors.grey, child: const Center(child: Text('Select an image'),),)
-                  : Image.file(
-                      File(controller.selectedImagePath.value),
-                      fit: BoxFit.cover,
-                      width: 450,
-                      height: 400,
-                    )),
-              // gallery image picker
-              Positioned(
-                top: 300,
-                right: 20,
-                child: FloatingActionButton(
-                  onPressed: () {
-                    controller.getImage(ImageSource.gallery);
-                  },
-                  backgroundColor: Colors.amber,
-                  child: const Icon(Icons.photo)
-                ),
-              ),
-              // camera image picker
-              Positioned( // camera image picker
-                top: 230,
-                right: 20,
-                child: FloatingActionButton(
-                  onPressed: () {
-                    controller.getImage(ImageSource.camera);
-                  },
-                  child: const Icon(Icons.camera_alt),
-                ),
-              )
-            ]),
-            const Text('To be implemented')
-          ],
-        ),
-      ),
+      body: ListView(children: <Widget>[
+        Stack(children: [
+          // spot for image to show
+          Obx(() => controller.selectedImagePath.value == ''
+              ? Container(
+                  width: 450,
+                  height: 390,
+                  color: Colors.grey,
+                  child: const Center(
+                    child: Text('Select an image'),
+                  ),
+                )
+              : Image.file(
+                  File(controller.selectedImagePath.value),
+                  fit: BoxFit.cover,
+                  width: 450,
+                  height: 390,
+                )),
+          // gallery image picker
+          Positioned(
+            top: 300,
+            right: 20,
+            child: FloatingActionButton(
+                onPressed: () {
+                  controller.getImage(ImageSource.gallery);
+                },
+                backgroundColor: Colors.amber,
+                child: const Icon(Icons.photo)),
+          ),
+          // camera image picker
+          Positioned(
+            // camera image picker
+            top: 230,
+            right: 20,
+            child: FloatingActionButton(
+              onPressed: () {
+                controller.getImage(ImageSource.camera);
+              },
+              child: const Icon(Icons.camera_alt),
+            ),
+          )
+        ]),
+        const Padding(padding: EdgeInsets.only(top: 20)),
+        const TextField(
+          decoration: InputDecoration(
+              hintText: 'Dish Name', border: OutlineInputBorder()),
+        )
+      ]),
     );
   }
 }
