d17c55c49db851ce4493ecc8952b5c7e4e9a3136
Wallace Phua <wallace@wallaces-air.lan>
2024-05-27 06:32:09 +0800

    finished adding firebasebase packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(const MyApp());
