93b03abf5f321125dfc62949aeb745d20823db76
Wallace Phua <wallace@wallaces-air.lan>
2024-05-30 07:11:30 +0800

    updated template homepage for testing

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,12 +1,28 @@
+import 'package:culinary_compass/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Home Page")
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Culinary Compass'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          TextButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Log Out'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          )
+        ], // appears on top right of AppBar
+      ),
     );
   }
 }
