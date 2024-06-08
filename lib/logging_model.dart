491c3fa6617d11f63124eaebdf3acad8bf5ddfa9
yewkhang <yewkhang45@outlook.com>
2024-06-08 14:18:50 +0800

    updated logging model for rating and description

--- lib/logging_model.dart
+++ lib/logging_model.dart
@@ -1,4 +1,4 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class LoggingModel {
@@ -6,19 +6,25 @@ class LoggingModel {
   final Image picture;
   final String name;
   final String location;
+  final double rating;
+  final String description;
 
   const LoggingModel({
     this.id,
     required this.picture,
     required this.name,
     required this.location,
+    required this.rating,
+    required this.description
   });
 
   toJson() {
     return {
       "Picture": picture,
       "Name": name,
-      "Location": location
+      "Location": location,
+      "Rating": rating,
+      "Description": description,
     };
   }
 }
