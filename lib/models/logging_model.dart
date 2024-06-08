d3e7631cc2f9b38ef15095985292c6cfe65cfa0d
yewkhang <yewkhang45@outlook.com>
2024-06-08 17:13:18 +0800

    updated logging model with required inputs

--- lib/models/logging_model.dart
+++ lib/models/logging_model.dart
@@ -1,16 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-
 class LoggingModel {
-  final String? id;
-  final Image picture;
+  final String? uid;
+  final String picture;
   final String name;
   final String location;
   final double rating;
   final String description;
 
   const LoggingModel({
-    this.id,
+    this.uid,
     required this.picture,
     required this.name,
     required this.location,
@@ -18,7 +15,9 @@ class LoggingModel {
     required this.description
   });
 
-  toJson() {
+  static LoggingModel empty() => LoggingModel(uid: '', picture: '', name: '', location: '', rating: 0.0, description: '');
+
+  Map<String, dynamic> toJson() {
     return {
       "Picture": picture,
       "Name": name,
