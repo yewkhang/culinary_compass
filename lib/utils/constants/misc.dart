366fe91367ab655858f7638d465ecc18f9de836c
Wallace Phua <wallace@wallaces-air.lan>
2024-06-23 22:25:56 +0800

    added methods for default textbox for future use, and other methods of auth

--- lib/utils/constants/misc.dart
+++ lib/utils/constants/misc.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// for Textboxes
+// Decoration for Textboxes
 // note: Color.fromRGBO() is used as constant value
 // input hintText by chaining with .copyWith() in other files
 const textInputDecoration = InputDecoration(
@@ -13,3 +13,61 @@ const textInputDecoration = InputDecoration(
     borderSide: BorderSide(color: Colors.black, width: 2.0)
   )
 );
+
+class DefaultTextFormField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const DefaultTextFormField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      //initialValue: email,
+      decoration: textInputDecoration.copyWith(hintText: hintText),
+      // returns null --> means is valid
+      // can validate more
+      validator: (typedEmail) => typedEmail!.isEmpty ? "Enter an email" : null,
+      onChanged: (typedEmail) {
+      //  setState(() => email = typedEmail);
+      },
+    );
+  }
+}
+
+class OtherSignInMethods extends StatelessWidget {
+
+  final String imagePath;
+  final Function()? onTap;
+
+  const OtherSignInMethods({
+    super.key,
+    required this.imagePath,
+    required this.onTap
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.grey[200]
+        ),
+        height: 70,
+        child: Image.asset(
+          imagePath,
+        ),
+      ),
+    );
+  }
+}
