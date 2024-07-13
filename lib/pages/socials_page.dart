80367b3a0ea0da29ac776a69d57eb2b960f3dd62
yewkhang <yewkhang45@outlook.com>
2024-07-13 18:31:05 +0800

    autofill friends username + display on groups page

--- lib/pages/socials_page.dart
+++ lib/pages/socials_page.dart
@@ -1,96 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:culinary_compass/utils/constants/colors.dart';
+import 'package:culinary_compass/utils/constants/sizes.dart';
 import 'package:culinary_compass/utils/controllers/friendsdialog_controller.dart';
+import 'package:culinary_compass/utils/controllers/groups_controller.dart';
 import 'package:culinary_compass/utils/controllers/profile_controller.dart';
+import 'package:culinary_compass/utils/controllers/tags_controller.dart';
+import 'package:culinary_compass/utils/custom_widgets.dart';
+import 'package:culinary_compass/utils/theme/elevated_button_theme.dart';
+import 'package:culinary_compass/utils/theme/textfield_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:get/get.dart';
 
 // ----------------------- DEFAULT SOCIAL PAGE ----------------------- //
 class SocialsPage extends StatelessWidget {
-  
   SocialsPage({super.key});
 
   final ProfileController profileController = Get.put(ProfileController());
-  final FriendsDialogController friendsDialogController = Get.put(FriendsDialogController());
+  final FriendsDialogController friendsDialogController =
+      Get.put(FriendsDialogController());
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          titleSpacing: 20.0,
-          title: const Text(
-            "Social",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)
-          ),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: "Friends"),
-              Tab(text: "Groups"),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            FriendsList(),
-            GroupsList(),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Get.dialog(AddFriendsCreateGroupsDialog());
-          },
-          backgroundColor: Colors.purple[200],
-          child: const Icon(Icons.person_add)
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      )
-    );
-  }
-}
-// ----------------------- DEFAULT SOCIAL PAGE ----------------------- //
-
-
-// ----------------------- DEFAULT POP-UP DIALOG ----------------------- //
-class AddFriendsCreateGroupsDialog extends StatelessWidget {
-
-  const AddFriendsCreateGroupsDialog({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      child: DefaultTabController(
         length: 2,
         child: Scaffold(
           appBar: AppBar(
-            centerTitle: true,
-            title: const Text(
-              "Choose Action",
-              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 22.0)
-            ),
+            titleSpacing: 20.0,
+            title: const Text("Social",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0)),
             bottom: const TabBar(
               tabs: [
-                Tab(text: "Add Friends"),
-                Tab(text: "Create Groups"),
+                Tab(text: "Friends"),
+                Tab(text: "Groups"),
               ],
             ),
           ),
           body: TabBarView(
             children: [
-              AddFriendsDialog(),
-              const CreateGroupDialog(),
+              FriendsList(),
+              GroupsList(),
             ],
-          )
-        )
-      )
-    );
+          ),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                Get.dialog(AddFriendsCreateGroupsDialog());
+              },
+              backgroundColor: Colors.purple[200],
+              child: const Icon(Icons.person_add)),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        ));
+  }
+}
+// ----------------------- DEFAULT SOCIAL PAGE ----------------------- //
+
+// ----------------------- DEFAULT POP-UP DIALOG ----------------------- //
+class AddFriendsCreateGroupsDialog extends StatelessWidget {
+  const AddFriendsCreateGroupsDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+        child: DefaultTabController(
+            length: 2,
+            child: Scaffold(
+                appBar: AppBar(
+                  centerTitle: true,
+                  title: const Text("Choose Action",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 22.0)),
+                  bottom: const TabBar(
+                    tabs: [
+                      Tab(text: "Add Friends"),
+                      Tab(text: "Create Groups"),
+                    ],
+                  ),
+                ),
+                body: TabBarView(
+                  children: [
+                    AddFriendsDialog(),
+                    CreateGroupDialog(),
+                  ],
+                ))));
   }
 }
 // ----------------------- DEFAULT POP-UP DIALOG ----------------------- //
 
-
 // ----------------------- FRIENDS TAB ----------------------- //
 class FriendsList extends StatelessWidget {
-
   FriendsList({super.key});
 
   final ProfileController profileController = ProfileController.instance;
@@ -111,16 +108,16 @@ class FriendsList extends StatelessWidget {
                 child: const Icon(Icons.person, color: Colors.white),
               ),
             ),
-            title: Text(profileController.user.value.friendsUsername.elementAt(index)),
+            title: Text(
+                profileController.user.value.friendsUsername.elementAt(index)),
             trailing: IconButton(
               icon: const Icon(Icons.delete),
               onPressed: () async {
-                await profileController.deleteFriendFromList(profileController.user.value.friendsUsername.elementAt(index));
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Friend successfully removed!")
-                  )
-                );
+                await profileController.deleteFriendFromList(profileController
+                    .user.value.friendsUsername
+                    .elementAt(index));
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text("Friend successfully removed!")));
               },
             ),
           );
@@ -131,48 +128,46 @@ class FriendsList extends StatelessWidget {
 }
 // ----------------------- FRIENDS TAB ----------------------- //
 
-
-
 class GroupsList extends StatelessWidget {
-  
-  const GroupsList({super.key});
+  GroupsList({super.key});
+
+  final GroupsController groupsController = Get.put(GroupsController());
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 5, ////////////////////////////////////// add actual logic later
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.purple,
-            child: GestureDetector(
-              onTap: () {
-                Get.dialog(CreateGroupDialog());
-              },
-              child: const Icon(Icons.group, color: Colors.white),
-            ),
-          ),
-          title: const Text("Group Name"),
-          trailing: IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              ////////////////////////////////////////// add delete groups functionality
-            },
-          ),
-        );
-      },
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: groupsController.fetchAllUserGroups(),
+        builder: (context, snapshot) {
+          return (snapshot.connectionState == ConnectionState.waiting)
+              ? const Center(
+                  // Retrieving from Firestore
+                  child: CircularProgressIndicator(
+                    color: CCColors.primaryColor,
+                  ),
+                )
+              : ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    String groupID = snapshot.data!.docs[index].id;
+                    var data = snapshot.data!.docs[index].data()
+                        as Map<String, dynamic>;
+                    return ListTile(
+                      title: Text(data['Name']),
+                    );
+                  });
+        });
   }
 }
 
-
 // ----------------------- DIALOG: ADDING FRIENDS ----------------------- //
 class AddFriendsDialog extends StatelessWidget {
-
   AddFriendsDialog({super.key});
 
   final ProfileController profileController = ProfileController.instance;
-  final FriendsDialogController friendsDialogController = FriendsDialogController.instance;
+  final FriendsDialogController friendsDialogController =
+      FriendsDialogController.instance;
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -184,9 +179,8 @@ class AddFriendsDialog extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-
-            const Text("Add Friends", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-
+            const Text("Add Friends",
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
             TextFormField(
               controller: friendsDialogController.friendUIDTextField,
               decoration: const InputDecoration(hintText: "Enter UID here"),
@@ -198,25 +192,23 @@ class AddFriendsDialog extends StatelessWidget {
                 }
                 return null;
               },
-              onChanged:(text) async {
+              onChanged: (text) async {
                 await friendsDialogController.validateUIDExists(text);
               },
             ),
-
             const SizedBox(height: 20.0),
-
             ElevatedButton(
               onPressed: () async {
-                if (_formKey.currentState!.validate() && friendsDialogController.uidExists.value) {
-                  await profileController.addFriendToList(friendsDialogController.friendUIDTextField.text);
+                if (_formKey.currentState!.validate() &&
+                    friendsDialogController.uidExists.value) {
+                  await profileController.addFriendToList(
+                      friendsDialogController.friendUIDTextField.text);
                   Navigator.pop(context);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text("Friend's UID successfully added!")
-                    )
-                  );
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("Friend's UID successfully added!")));
                 }
-                friendsDialogController.friendUIDTextField.text = ""; // clear controller
+                friendsDialogController.friendUIDTextField.text =
+                    ""; // clear controller
               },
               child: const Text("Add Friend"),
             ),
@@ -228,19 +220,16 @@ class AddFriendsDialog extends StatelessWidget {
 }
 // ----------------------- DIALOG: ADDING FRIENDS ----------------------- //
 
+class CreateGroupDialog extends StatelessWidget {
+  CreateGroupDialog({super.key});
 
-class CreateGroupDialog extends StatefulWidget {
-  
-  const CreateGroupDialog({super.key});
-
-  @override
-  State<CreateGroupDialog> createState() => _CreateGroupDialogState();
-}
-
-class _CreateGroupDialogState extends State<CreateGroupDialog> {
-
-  bool isChecked = false;
-  final FriendsDialogController friendsDialogController = FriendsDialogController.instance;
+  final FriendsDialogController friendsDialogController =
+      FriendsDialogController.instance;
+  final ProfileController profileController = ProfileController.instance;
+  final TextEditingController groupNameController = TextEditingController();
+  final TextEditingController userSearchController = TextEditingController();
+  final GroupsController groupsController = Get.put(GroupsController());
+  final TagsController nameTagsController = Get.put(TagsController());
 
   @override
   Widget build(BuildContext context) {
@@ -249,42 +238,71 @@ class _CreateGroupDialogState extends State<CreateGroupDialog> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-
-          const Text("Create Group", style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-
-          const TextField(
-            decoration: InputDecoration(hintText: "Group Name"),
+          const Text("Create Group",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+          TextField(
+            controller: groupNameController,
+            decoration: const InputDecoration(hintText: "Group Name"),
           ),
-
           const SizedBox(height: 20.0),
-
-          const Text("Who would you like to add?"),
-
-          const TextField(
-            decoration: InputDecoration(hintText: "Enter Friend's Username"),
-            // onChanged: ,
+          // ----- SELECTED FRIENDS DISPLAY ----- //
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: CCSizes.defaultSpace),
+            child: Obx(() => nameTagsController.selectedFriendsNames.isEmpty
+                ? const Center(
+                    child: Text('Add friends'),
+                  )
+                : Wrap(
+                    children: nameTagsController.selectedFriendsNames
+                        .map((element) => CCTagsContainer(
+                            label: Text(element),
+                            deleteIcon: const Icon(Icons.clear),
+                            onDeleted: () => nameTagsController.selectedFriendsNames
+                                .remove(element)))
+                        .toList(),
+                  )),
           ),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: 5, ////////////////////////////////////// add actual logic later
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text("Name ${index + 1}"),
-                  value: isChecked,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked = value!;
-                    });
-                  },
-                );
-              },
-            ),
+          Padding(
+            padding: const EdgeInsets.all(CCSizes.defaultSpace),
+            child: TypeAheadField(
+                controller: userSearchController,
+                builder: (context, controller, focusNode) {
+                  return TextField(
+                      controller: controller,
+                      focusNode: focusNode,
+                      decoration: textFieldInputDecoration(
+                          hintText: 'Enter username',
+                          prefixIcon: Icons.people_alt));
+                },
+                itemBuilder: (BuildContext context, String itemData) {
+                  return ListTile(
+                    title: Text(itemData),
+                    tileColor: Colors.white,
+                  );
+                },
+                onSelected: (String suggestion) {
+                  // add friends only if selected friends doesn't contain 'suggestion'
+                  if (nameTagsController.selectedFriendsNames.contains(suggestion) ==
+                      false) {
+                    nameTagsController.selectedFriendsNames.add(suggestion);
+                    // clear text field upon selection of a friend
+                    userSearchController.clear();
+                  }
+                },
+                suggestionsCallback: (String query) {
+                  // list of user's friends to select from
+                  return groupsController.getFriendSuggestions(
+                      query, profileController.user.value.friendsUsername);
+                }),
           ),
           const SizedBox(height: 20.0),
           ElevatedButton(
-            onPressed: () {
-              ////////////////////////////////////////////////////// create group logic
+            style: CCElevatedTextButtonTheme.lightInputButtonStyle,
+            onPressed: () async {
+              await groupsController.createGroup(groupNameController.text,
+                  ['members UID'], nameTagsController.selectedFriendsNames);
+              Get.back();
             },
             child: const Text("Create Group"),
           ),
